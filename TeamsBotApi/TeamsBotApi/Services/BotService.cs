c4868e5b04eda213faac6ccd56fd177eb8de24e9
Steve Yeager <steve.yeager@littlebytegames.com>
2020-11-12 21:07:03 -0800

    Add /randomNumber command

--- TeamsBotApi/TeamsBotApi/Services/BotService.cs
+++ TeamsBotApi/TeamsBotApi/Services/BotService.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Builder.Teams;
@@ -13,7 +14,35 @@ namespace TeamsBotApi.Services
             turnContext.Activity.RemoveRecipientMention();
             var text = turnContext.Activity.Text.Trim().ToLower();
 
-            var replyText = $"Did you say: {text}?";
+            if(text.StartsWith("/"))
+            {
+                await ProcessCommandAsync(text, turnContext, cancellationToken);
+            }
+            else
+            {
+                var replyText = $"Did you say: {text}?";
+                await turnContext.SendActivityAsync(MessageFactory.Text(replyText, replyText), cancellationToken);
+            }
+        }
+
+        private async Task ProcessCommandAsync(string text, ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var split = text.Split();
+
+            var replyText = "That command is not supported";
+
+            switch(split[0])
+            {
+                case "/randomnumber":
+                    var min = int.Parse(split[1]);
+                    var max = int.Parse(split[2]);
+
+                    var random = new Random();
+                    var value = random.Next(min, max);
+                    replyText = $"You rolled a '{value}'";
+                    break;
+            }
+
             await turnContext.SendActivityAsync(MessageFactory.Text(replyText, replyText), cancellationToken);
         }
     }
