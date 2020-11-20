777f2c806e97973c682c3999836714485fbe65a3
Steve Yeager <steve.yeager@littlebytegames.com>
2020-11-19 16:49:14 -0800

    Add simple logging

--- TeamsBotApi/TeamsBotApi/Controllers/BotController.cs
+++ TeamsBotApi/TeamsBotApi/Controllers/BotController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.Extensions.Logging;
 
 namespace TeamsBotApi.Controllers
 {
@@ -11,16 +12,19 @@ namespace TeamsBotApi.Controllers
     {
         private readonly IBotFrameworkHttpAdapter adapter;
         private readonly IBot bot;
+        private readonly ILogger<BotController> logger;
 
-        public BotController(IBotFrameworkHttpAdapter adapter, IBot bot)
+        public BotController(IBotFrameworkHttpAdapter adapter, IBot bot, ILogger<BotController> logger)
         {
             this.adapter = adapter;
             this.bot = bot;
+            this.logger = logger;
         }
 
         [HttpPost]
         public async Task PostAsync()
         {
+            logger.LogInformation("Recieved Teams message.");
             await adapter.ProcessAsync(Request, Response, bot);
         }
     }
--- TeamsBotApi/TeamsBotApi/Services/CommandService.cs
+++ TeamsBotApi/TeamsBotApi/Services/CommandService.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using BuildSystem.Api;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Schema;
+using Microsoft.Extensions.Logging;
 
 namespace TeamsBotApi.Services
 {
@@ -11,16 +12,19 @@ namespace TeamsBotApi.Services
         private readonly CommandParser commandParser;
         private readonly NotificationService notificationService;
         private readonly BuildFacade buildFacade;
+        private readonly ILogger<CommandService> logger;
 
-        public CommandService(CommandParser commandParser, NotificationService notificationService, BuildFacade buildFacade)
+        public CommandService(CommandParser commandParser, NotificationService notificationService, BuildFacade buildFacade, ILogger<CommandService> logger)
         {
             this.commandParser = commandParser;
             this.notificationService = notificationService;
             this.buildFacade = buildFacade;
+            this.logger = logger;
         }
 
         public async Task ProcessCommandAsync(string message, ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
         {
+            logger.LogInformation("Processing Command.");
             var command = commandParser.Parse(message);
             await command.ExecuteAsync(buildFacade, notificationService, message, turnContext, cancellationToken);
         }
